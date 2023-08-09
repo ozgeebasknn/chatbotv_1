8b2dfbb492461a569f8a1edb9b1168da5c21a75d
ozge <baskinozge24@gmail.com>
2023-08-09 15:06:00 +0300

    recyler view

--- app/src/main/java/com/example/chatbotv_1/MainActivity.java
+++ app/src/main/java/com/example/chatbotv_1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.chatbotv_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -9,21 +10,33 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     TextView welcomeTextView;
     EditText messageEditText;
     ImageButton sendButton;
+    List<Message> messageList;
+    MessageAdapter messageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        messageList = new ArrayList<>();
 
         recyclerView = findViewById(R.id.recycle_view);
         welcomeTextView = findViewById(R.id.welcome_text);
         messageEditText = findViewById(R.id.message_edit_text);
         sendButton = findViewById(R.id.send_btn);
+
+        messageAdapter = new MessageAdapter(messageList);
+        recyclerView.setAdapter(messageAdapter);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setStackFromEnd(true);
+        recyclerView.setLayoutManager(llm);
 
         sendButton.setOnClickListener((v)->{
             String question = messageEditText.getText().toString().trim();
