eb96c074fc664cb9bd63177aec2b52334d6b7d41
ozge <baskinozge24@gmail.com>
2023-08-10 14:07:45 +0300

    integrate OpenAI API

--- app/src/main/java/com/example/chatbotv_1/MainActivity.java
+++ app/src/main/java/com/example/chatbotv_1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.chatbotv_1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,8 +12,21 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -21,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
     ImageButton sendButton;
     List<Message> messageList;
     MessageAdapter messageAdapter;
+    public static final MediaType JSON
+            = MediaType.get("application/json;charset=utf-8");
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
             String question = messageEditText.getText().toString().trim();
             addToChat(question,Message.SENT_BY_ME);
             messageEditText.setText("");
+            callAPI(question);
             welcomeTextView.setVisibility(View.GONE);
         });
     }
@@ -56,5 +74,55 @@ public class MainActivity extends AppCompatActivity {
                 recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
             }
         });
+    }
+
+    void addResponse(String response){
+        addToChat(response,Message.SENT_BY_BOT);
+    }
+
+    void callAPI(String question){
+
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("model", "text-davinci-003");
+            jsonBody.put("prompt",question);
+            jsonBody.put("max-tokens",4000);
+            jsonBody.put("temperature",0);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/completions")
+                .header("Authorization","Bearer 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")
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                addResponse("Failed to load response due to" +e.getMessage());
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(response.body().string());
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        String result = jsonArray.getJSONObject(0).getString("text");
+                        addResponse(result.trim());
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+            }
+        });
+
     }
 }
